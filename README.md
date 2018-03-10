81ef986c6585629dff4c80f9d3a4b70adad2c974
ChaikaAM <User543>
2018-03-10 15:37:11 +0300

    Discovery server (Netflix OSS Eureka)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # springcloud
-Project for students
+## Part 1
+
+Discovery server is provided (Netflix OSS Eureka).
+To run application enter in the root:
+
+``mvn -pl discovery_server clean spring-boot:run
+``
+
---
+++ discovery_server/src/main/java/com/springcloud/discovery/controllers/AboutController.java
@@ -0,0 +1,21 @@
+package com.springcloud.discovery.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AboutController {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @RequestMapping("/about")
+    public String getAbout() {
+        return "Application name : " + boiledString(applicationName);
+    }
+
+    private String boiledString(String text) {
+        return "<b>\"" + text + "\"</b>";
+    }
+}
