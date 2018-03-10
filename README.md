65d67caa68cae24f5acf5842c265b66aa4dd73b1
ChaikaAM <User543>
2018-03-10 18:18:45 +0300

    Configurations management

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # springcloud
+
+## Part 2
+
+Configuration server management is implemented (with local config files, could be switched to remote configs repository). 
+To run config server enter in the root:
+
+``mvn -pl config_server clean spring-boot:run``
+
+Then just start discovery server and make sure that configurations would be fetched from configuration server.
+
+``mvn -pl discovery_server clean spring-boot:run
+``
+
+To refresh configs change em in config server and `POST host/refresh` on client of config server
+
 ## Part 1
 
 Discovery server is provided (Netflix OSS Eureka).
--- config_server/src/main/java/com/springcloud/config/controllers/AboutController.java
+++ config_server/src/main/java/com/springcloud/config/controllers/AboutController.java
@@ -1,28 +1,18 @@
-package com.springcloud.discovery.controllers;
+package com.springcloud.config.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope   //POST host/refresh
 public class AboutController {
 
     @Value("${spring.application.name}")
     private String applicationName;
 
-
-    @Value("${greeting: ((((( SORRY. Somehow I dont use configs from configuration server}")
-    public String greeting;
-
     @RequestMapping("/about")
     public String getAbout() {
-
-        return String.join(
-                ", ",
-                "Application name : " + boiledString(applicationName),
-                "Greeting : " + boiledString(greeting));
+        return "Application name : " + boiledString(applicationName);
     }
 
     private String boiledString(String text) {
